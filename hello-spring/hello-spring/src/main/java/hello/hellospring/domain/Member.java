47e60e4a792afa65995280121dee4ea3989c292e
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-04-29 17:35:16 +0900

    04.29

--- hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    // ID(번호) 생성 어노테이션
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 시스템이 저장한 아이디 -> 데이터 구분을 위해
+
     private String name;
 
     public Long getId() {
