f3ea135356639479467e35cd10586cb4fe0f4c13
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-05-11 13:39:49 +0900

    05.11

--- core/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -30,7 +30,7 @@ public class ConfigurationSingletonTest {
     @Test
     void configurationDeep() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-        
+
         //AppConfig도 스프링 빈으로 등록된다.
         AppConfig bean = ac.getBean(AppConfig.class);
         System.out.println("bean = " + bean.getClass());
