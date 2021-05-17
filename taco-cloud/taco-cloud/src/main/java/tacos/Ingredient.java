66cf9b580e4492c1d89be0b7809c51f251e00b77
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-05-17 17:55:20 +0900

    05.17

--- taco-cloud/taco-cloud/src/main/java/tacos/Ingredient.java
+++ taco-cloud/taco-cloud/src/main/java/tacos/Ingredient.java
@@ -1,22 +1,20 @@
 package tacos;
 
 import lombok.Data;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-
 
 @Data
 // @RequiredArgsConstructor : 생성자를 만들어준다
 @RequiredArgsConstructor
-@Getter
-@Setter
+
 public class Ingredient {
 
+    // 식자재를 나타내는데 필요한 속성
     private final String id;
     private final String name;
     private final Type type;
 
+    // 열거형 타입
     public static enum Type{
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
