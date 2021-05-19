943b51eb235dfcf409c81ffefdd30ca1520d89c2
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-05-19 23:38:56 +0900

    05.19

--- core/core/src/test/java/hello/core/autowired/AllBeanTest.java
+++ core/core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -3,7 +3,8 @@ import hello.core.AutoAppConfig;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import java.util.List;
@@ -20,6 +21,7 @@ public class AllBeanTest {
         // 할인서비스
         DiscountService discountService = ac.getBean(DiscountService.class);
         Member member = new Member(1L, "userA", Grade.VIP);
+
         int discountPrice = discountService.discount(member, 10000,
                 "fixDiscountPolicy");
         assertThat(discountService).isInstanceOf(DiscountService.class);
